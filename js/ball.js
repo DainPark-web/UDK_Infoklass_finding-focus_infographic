d3f57de0b3075b0f6349bbd72e394d78becb38ee
dain <57316771+DainPark-web@users.noreply.github.com>
2022-01-20 17:23:14 +0100

    ypddate

--- js/ball.js
+++ js/ball.js
@@ -1,14 +1,19 @@
 class Ball {
     constructor(dis, size){
-        this.pos = createVector(50, dis);
+        this.pos = createVector(dis, 50);
         this.size = size;
     }
 
-    draw(){
+    draw(j, i, d){
+        push();
+        colorMode(HSB, 400);
         strokeWeight(1);
-        stroke(255);
-        fill(0);
-        circle(this.pos.x, this.pos.y, this.size);
+        stroke(0);
+        fill(309, 60, d ? 400 : 0 );
+        // noFill();
+        circle(this.pos.x + 50 , this.pos.y * j + 50 , this.size);
+        pop();
+        
     }
 
     update(){
